aede780d702641ef793fd8df673bebca3ecaa33d
Maksim shachykau <50798581+MaksimShachykau@users.noreply.github.com>
2023-09-06 15:20:06 +0200

    B-12: update Rating component

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { KeyboardEvent, useEffect, useState } from 'react';
 import cn from 'classnames';
 
 import { IRatingProps } from './Rating.props';
@@ -6,30 +6,66 @@ import StarIcon from './star.svg';
 
 import styles from './Rating.module.css';
 
-const Rating = ({ rating, ...props } : IRatingProps): JSX.Element => {
+const Rating = ({ rating, isEditable, setRating, ...props } : IRatingProps): JSX.Element => {
     const [ ratingArray, setRatingArray ] = useState<JSX.Element[]>(new Array(5).fill(<></>));
 
     useEffect(() => {
-        someFunk(rating);
+        constructRating(rating);
     }, [rating]);
 
-    const someFunk = (rating :number): void => {
+    const constructRating = (currentRating :number): void => {
         const updatedArray = ratingArray.map((e: JSX.Element, i: number) => {
             return (
-                <StarIcon key={i} className={cn(
-                    styles.star,
-                    {
-                        [styles.fill]: i < rating,
-                    }
-                )}/>
+                <span
+                    key={i}
+                    className={cn(
+                        styles.star,
+                        {
+                            [styles.fill]: i < currentRating,
+                            [styles.pointed]: isEditable,
+                        }
+                    )}
+                    onMouseEnter={() => changeDisplay(i + 1)}
+                    onMouseLeave={() => changeDisplay(rating)}
+                    onClick={() => onClick(i + 1)}
+                >
+                    <StarIcon
+                        tabIndex={isEditable ? 0 : -1}
+                        onKeyDown={(e: KeyboardEvent<SVGAElement>) => isEditable && handleSpace(i + 1, e)}
+                    />
+                </span>
             );
         });
         setRatingArray(updatedArray);
     };
 
+    const changeDisplay = (i: number) => {
+        if(!isEditable) {
+            return;
+        }
+
+        constructRating(i);
+    };
+
+    const onClick = (i: number) => {
+        if(!isEditable || !setRating) {
+            return;
+        }
+
+        setRating(i);
+    };
+
+    const handleSpace = (i: number, e: KeyboardEvent<SVGAElement>) => {
+        if(e.code !== 'Space' || !setRating) {
+            return;
+        }
+
+        setRating(i);
+    };
+
     return(
         <div {...props}>
-            {ratingArray}
+            {ratingArray.map((e: JSX.Element, i: number) => <span key={i}>{e}</span>)}
         </div>
     );
 };
