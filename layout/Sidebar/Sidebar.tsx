89da01280f5349283d6c8e176718526676f986dd
Maksim shachykau <50798581+MaksimShachykau@users.noreply.github.com>
2023-09-15 10:25:22 +0200

    B-24: add styles to Sidebar component

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import cn from 'classnames';
 
 import { ISidebarProps } from './Sidebar.Props';
+import Logo from '../../icons/Logo.svg';
+
+import styles from './Sidebar.module.css';
 import Menu from '../Menu';
 
-const Sidebar = ({ ...props }: ISidebarProps): JSX.Element => {
+const Sidebar = ({ className, ...props }: ISidebarProps): JSX.Element => {
     return(
-        <div {...props}>
+        <div className={cn(className, {
+            [styles['sidebar']]: true
+        })} {...props}>
+            <Logo className={styles['logo']} />
+            <>Search</>
             <Menu />
         </div>
     );
