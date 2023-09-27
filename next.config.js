0aa2692c257e24091fc66a40bd5a623b55d0569c
Maksim shachykau <50798581+MaksimShachykau@users.noreply.github.com>
2023-09-27 10:00:02 +0200

    B-40: add Image from next

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    domains: ['courses-top.ru']
+  },
   reactStrictMode: true,
   webpack: (config) => {
     config.module.rules.push({
