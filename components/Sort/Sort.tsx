5264687cdde408439200c7edad925f22178f0931
Maksim shachykau <50798581+MaksimShachykau@users.noreply.github.com>
2023-10-10 18:25:30 +0200

    B-59: add tabIndex to Sort

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -1,3 +1,4 @@
+import { KeyboardEvent } from 'react';
 import cn from 'classnames';
 import { ISortProps, SortEnum } from "./Sort.props";
 import SortIcon from './Sort.svg';
@@ -5,9 +6,18 @@ import SortIcon from './Sort.svg';
 import styles from './Sort.module.css';
 
 const Sort = ({ sort, setSort, className, ...props }: ISortProps):JSX.Element => {
+    const sortByKey = (key: KeyboardEvent, sortEnum: SortEnum) => {
+        if(key.code === 'Enter' || key.code === 'Space') {
+            key.preventDefault();
+            setSort(sortEnum);
+        }
+    };
+
     return(
         <div className={cn(styles.sort, className)} {...props}>
             <span
+                tabIndex={0}
+                onKeyDown={(k: KeyboardEvent) => sortByKey(k, SortEnum.ByRate)}
                 onClick={() => setSort(SortEnum.ByRate)}
                 className={cn(styles['sortBy'], {
                     [styles['isActive']]: sort === SortEnum.ByRate
@@ -16,6 +26,8 @@ const Sort = ({ sort, setSort, className, ...props }: ISortProps):JSX.Element =>
                 <SortIcon/>by&nbsp;rating
             </span>
             <span
+                tabIndex={0}
+                onKeyDown={(k: KeyboardEvent) => sortByKey(k, SortEnum.byPrice)}
                 onClick={() => setSort(SortEnum.byPrice)}
                 className={cn(styles['sortBy'], {
                     [styles['isActive']]: sort === SortEnum.byPrice
